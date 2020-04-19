77115e7fc1a34ff12b65835a7e7b1a8cae6876f3
aurel fontaine <ajfame11@gmail.com>
2020-04-19 22:39:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ class TicTacToe
   def turn_count
     turn = 0
     @board.each do |turn|
-      if turn = "X" || turn = "O"
+      if turn == "X" || turn == "O"
         turn += 1
       end
     end
