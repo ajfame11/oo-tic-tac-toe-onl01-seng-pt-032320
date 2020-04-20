2f3296738ea9e56db19cab3737ab534b54f37443
aurel fontaine <ajfame11@gmail.com>
2020-04-20 00:12:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -111,6 +111,16 @@ class TicTacToe
     end
   end
 
-  
+  def play
+    until over? == true
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 
 end
